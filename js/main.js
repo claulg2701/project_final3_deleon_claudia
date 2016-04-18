b1cf9a8d0cb77a99a7790bd73a21285dde8fdb58
Claudia De Leon <claulg2701@gmail.com>
2016-04-17 23:34:11 -0400

    sometimes a plugin is not the answer...

--- js/main.js
+++ js/main.js
@@ -101,4 +101,13 @@ jQuery( document ).ready( function() {
 
         } );
 
+        /* enable menu tabs */
+        $( 'ul.tabs li' ).click( function() {
+            var tabId = $( this ).attr( 'data-tab' );
+		    $( 'ul.tabs li' ).removeClass( 'current' );
+		    $( ' .tab-content' ).removeClass( 'current' );
+		    $( this ).addClass( 'current' );
+		    $( '#' + tabId ).addClass( 'current' );
+    } );
+
 } ); /* end of ready funtcion */
